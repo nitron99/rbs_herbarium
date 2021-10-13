b1825aa367e0a8f71fb614e384b46c16c323de7b
Nitin Singh <nitin99bop2codf@gmail.com>
2021-10-13 22:36:21 +0530

    about page

--- src/components/pages/about_page/about.js
+++ src/components/pages/about_page/about.js
@@ -9,7 +9,24 @@ class about extends Component{
             <div className="ABOUT">
             
                 
-                this is about page
+                t<div className="about_written_content">
+                        <div className="about_para1">
+                            <em >Nature Does Not Hurry, Yet Everything Is Accomplished</em><br/><br/>The R.B.S. Herbarium contains over 500 specimens of vascular plants which have
+                                been preserved in digital as well as real form. The Herbarium has been published in
+                                the “Flora of Agra” (BSI, Dehradun) {"&"} is available for reference..
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                                irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                                deserunt mollit anim id est laborum.
+                        </div>
+
+
+                    there will be collapsable cards
+                    
+                       
+                </div>
 
             </div>
         );
